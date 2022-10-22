f1699e1355e811fffa190a232ca05dea161bdebe
gabrielcavalcante-cs <gabriel.lcifba@gmail.com>
2022-10-22 18:00:55 -0300

    :wrench: commit

--- miauduino.c
+++ miauduino.c
@@ -1,3 +1,6 @@
+#include <HX711.h> // biblioteca para controlar balanca
+#define MIN_WEIGHT xxxx // nao eh 0, uma vez que o peso da propria tigela tambem sera computado
+
 void setup(void) {
 	// TODO
 }

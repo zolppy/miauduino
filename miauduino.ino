88db83d10f2e1d1ac11a8c83d5684050d5441552
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2022-10-23 00:12:18 -0300

    Update miauduino.ino

--- miauduino.ino
+++ miauduino.ino
@@ -1,5 +1,8 @@
 #include <HX711.h> // Modulo conversor e amplificador de sinais para celulas de carga
 #include <Servo.h> // Servo motor
+// Com macros de pre-processamentos, nao eh necessario alocar espaco na memoria, diferentemente de constantes
+// O compilador apenas substitui a macro pelo seu valor, durante o pre-processamento
+#define CALIBRATION_FACTOR xxxx
 #define MIN_WEIGHT xxxx // Nao eh 0, pois o peso da propria tigela tambem sera computado
 #define DOUT_PIN xxxx
 #define SCK_PIN xxxx
@@ -7,7 +10,6 @@
 HX711 load_cell;
 
 float weight;
-float calibration_factor = xxxx;
 
 void setup(void) {
   // A depender do modelo, nao sera necessario "setar" taxa de transmissao e configuracao para comunicacao
@@ -18,6 +20,6 @@ void setup(void) {
 }
 
 void loop(void) {
-  load_cell.set_scale(calibration_factor); // ajusta fator de calibracao
+  load_cell.set_scale(CALIBRATION_FACTOR); // ajusta fator de calibracao
   delay(xxxx); // Em ms (1000 ms == 1 s)
 }

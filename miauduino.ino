48c8b9626e4c7392def66c5ed4cc545697801e24
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2022-10-24 21:32:11 -0300

    :wrench: feat: commit

--- miauduino.ino
+++ miauduino.ino
@@ -1,8 +1,11 @@
 #include <HX711.h> // Modulo conversor e amplificador de sinais para celulas de carga
-#include <Servo.h> // Servo motor
-#define MIN_WEIGHT xxxx // Nao eh 0, pois o peso da propria tigela tambem sera computado
 #define DOUT_PIN xxxx
 #define SCK_PIN xxxx
+#define MIN_WEIGHT xxxx
+#define RELAY_PIN1 xxxx
+#define RELAY_PIN2 xxxx
+#define RELAY_PIN3 xxxx
+#define RELAY_PIN4 xxxx
 
 HX711 load_cell;
 
@@ -10,12 +13,36 @@ float weight;
 float calibration_factor = xxxx;
 
 void setup(void) {
-  load_cell.begin(DOUT_PIN, SCK_PIN); // Inicializa a celula de carga
-  load_cell.set_scale(); // Configura a escala da celula de carga
-  load_cell.tare(); // Zera a celula de carga
+  // Para rele:
+  // LOW: ligado
+  // HIGH: desligado
+  
+  // Define pinos de saida para o rele
+  pinMode(RELAY_PIN1, OUTPUT);
+  pinMode(RELAY_PIN2, OUTPUT);
+  pinMode(RELAY_PIN3, OUTPUT);
+  pinMode(RELAY_PIN4, OUTPUT);
+  
+  // Rele inicia desligado
+  digitalWrite(RELAY_PIN1, HIGH);
+  digitalWrite(RELAY_PIN2, HIGH);
+  digitalWrite(RELAY_PIN3, HIGH);
+  digitalWrite(RELAY_PIN4, HIGH);
+  
+  Serial.begin(9600); // Inicializa a comunicacao serial em 9600bps
+  
+  load_cell.begin(DOUT_PIN, SCK_PIN);
+  load_cell.set_scale();
+  load_cell.tare(); // Zera
 }
 
 void loop(void) {
-  load_cell.set_scale(calibration_factor); // ajusta fator de calibracao
+  load_cell.set_scale(calibration_factor); // Ajusta fator de calibracao
+  
+  if(weight == MIN_WEIGHT) {
+    // Rele ativa tensao para ligar motor espiral de liberacao de racao
+    digitalWrite(RELAY_PIN1, LOW);
+  }
+  
   delay(xxxx); // Em ms (1000 ms == 1 s)
 }

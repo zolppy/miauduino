da965808ec80f899a5b5e899742ba03ab74b5eb1
gabrielcavalcante-cs <gabriel.lcifba@gmail.com>
2022-10-29 13:34:10 -0300

    :wrench: feat: commit

--- miauduino.ino
+++ miauduino.ino
@@ -1,48 +1,63 @@
 #include <HX711.h> // Modulo conversor e amplificador de sinais para celulas de carga
-#define DOUT_PIN xxxx
-#define SCK_PIN xxxx
-#define MIN_WEIGHT xxxx
-#define RELAY_PIN1 xxxx
-#define RELAY_PIN2 xxxx
-#define RELAY_PIN3 xxxx
-#define RELAY_PIN4 xxxx
 
-HX711 load_cell;
+// Macros (macroinstrucoes) nao precisam armazenar valores na memoria
+#define CALIBRATION_FACTOR xxxx
+#define SCALE_DOUT_PIN xxxx
+#define SCALE_SCK_PIN xxxx
+#define RELAY_DOUT_PIN xxxx
+#define RELAY_SCK_PIN xxxx
+#define MIN_MASS xxxx
+#define MAX_MASS xxxx
 
-float weight;
-float calibration_factor = xxxx;
+HX711 scale;
+
+float mass;
 
 void setup(void) {
-  // Para rele:
-  // LOW: ligado
-  // HIGH: desligado
-  
   Serial.begin(9600); // Inicializa a comunicacao serial em 9600bps
   
-  // Define pinos de saida para o rele
-  pinMode(RELAY_PIN1, OUTPUT);
-  pinMode(RELAY_PIN2, OUTPUT);
-  pinMode(RELAY_PIN3, OUTPUT);
-  pinMode(RELAY_PIN4, OUTPUT);
+  // Configurando pinos de saida para o rele
+  pinMode(RELAY_DOUT_PIN, OUTPUT);
+  pinMode(RELAY_SCK_PIN, OUTPUT);
   
   // Rele inicia desligado
-  digitalWrite(RELAY_PIN1, HIGH);
-  digitalWrite(RELAY_PIN2, HIGH);
-  digitalWrite(RELAY_PIN3, HIGH);
-  digitalWrite(RELAY_PIN4, HIGH);
+  digitalWrite(RELAY_DOUT_PIN, HIGH);
+  digitalWrite(RELAY_SCK_PIN, HIGH);
   
-  load_cell.begin(DOUT_PIN, SCK_PIN);
-  load_cell.set_scale();
-  load_cell.tare(); // Zera
+  scale.begin(SCALE_DOUT_PIN, SCALE_SCK_PIN); // Configura pinos da celula de carga
+  //scale.power_up(); // Liga sensor
+
+  // Saida de dados no monitor serial
+  Serial.println(); // Pula uma linha
+  Serial.println("HX711 - Calibracao da celula de carga");
+  Serial.println("Remova a massa da celula de carga");
+  Serial.println("Depois que as leituras comecarem, coloque um massa conhecido sobre a celula de carga");
+
+  scale.set_scale(); // Limpa valor da celula de carga
+  zero_scale(); // Zera para desconsiderar massa da estrutura
+}
+
+void zero_scale(void) {
+  Serial.println(); // Pula uma linha
+  balanca.tare(); // Zera a celula de carga
+  Serial.println("Celula de carga, zerada ");  
 }
 
 void loop(void) {
-  load_cell.set_scale(calibration_factor); // Ajusta fator de calibracao
+  scale.set_scale(CALIBRATION_FACTOR); // Ajusta fator de calibracao
+
+  // Saida de dados no monitor serial
+  Serial.print("Massa: ");
+  Serial.print(balanca.get_units(), 3); // Imprime peso da celula de carga com 3 casas decimais
+  Serial.print(" kg");
+  Serial.print("      Fator de calibracao: ");
+  Serial.println(CALIBRATION_FACTOR);
+  delay(500); // Em ms (1000 ms == 1 s)
   
-  if(weight == MIN_WEIGHT) {
+  if(mass == MIN_MASS) {
     // Rele ativa tensao para ligar motor espiral de liberacao de racao
-    digitalWrite(RELAY_PIN1, LOW);
+    digitalWrite(RELAY_DOUT_PIN, LOW);
   }
   
-  delay(xxxx); // Em ms (1000 ms == 1 s)
+  delay(xxxx);
 }

a343820880b4cc6777a1da8a41d831b0b91ec973
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2022-11-26 18:46:09 -0300

    :bug: commit

--- miauduino.ino
+++ miauduino.ino
@@ -1,15 +1,14 @@
-#include <HX711.h> // Modulo da balanca
-#include <string>
+#include <HX711.h>
+#include <EEPROM.h>
 
 const int BPS = 9600;
 const int SECOND = 1000;
 const float MIN_WEIGHT = 0.320;
 
 // Pinos
-const string RELAY_PIN_1("A1");
-const string RELAY_PIN_2("A2");
-const string SCALE_SCK_PIN("A3");
-const string SCALE_DOUT_PIN("A4");
+const int RELAY_PIN = 1;
+const int SCALE_SCK_PIN = 2;
+const int SCALE_DOUT_PIN = 3;
 
 float weight;
 const float ACCEPTABLE_WEIGHT = 1.500;
@@ -19,10 +18,10 @@ HX711 scale;
 
 void setup(void) {
   Serial.begin(BPS);
+  scale.set_scale(CALIBRATION_FACTOR); // Ajusta fator de calibracao
   
   // Configuracao de pinos
-  pinMode(RELAY_PIN_1, OUTPUT);
-  pinMode(RELAY_PIN_2, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
   scale.begin(SCALE_DOUT_PIN, SCALE_SCK_PIN);
 
   Serial.println();
@@ -41,8 +40,6 @@ void zero_scale(void) {
 }
 
 void loop(void) {
-  scale.set_scale(CALIBRATION_FACTOR); // Ajusta fator de calibracao
-
   Serial.println();
   Serial.print("Peso: ");
   Serial.print(scale.get_units(), 3); // Imprime peso da balanca com 3 casas decimais
@@ -51,17 +48,15 @@ void loop(void) {
   Serial.println(CALIBRATION_FACTOR);
   delay(0.5 * SECOND);
 
-  weight = 0;
+  weight = get_units();
 
   while(weight < ACCEPTABLE_WEIGHT) {
-    digitalWrite(RELAY_PIN_1, LOW);
-    digitalWrite(RELAY_PIN_2, HIGH);
+    digitalWrite(RELAY_PIN, HIGH);
 
     delay(0.5 * SECOND);
 
     weight = get_units();
   }
 
-  digitalWrite(RELAY_PIN_1, HIGH);
-  digitalWrite(RELAY_PIN_2, LOW);
+  digitalWrite(RELAY_PIN, LOW);
 }
